5e513a670420f88b47f547cc14412248e188bcc2
Salvador Rioboo Naranjo <salrio74@gmail.com>
2025-03-11 08:48:05 +0100

    feat: allow message null to reuse operations

--- push_swap_utils.c
+++ push_swap_utils.c
@@ -6,7 +6,7 @@
 /*   By: srioboo- <srioboo-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 11:24:03 by srioboo-          #+#    #+#             */
-/*   Updated: 2025/03/10 14:55:08 by srioboo-         ###   ########.fr       */
+/*   Updated: 2025/03/11 08:46:57 by srioboo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@ void	show_op_msg(char *op)
 {
 	int	len;
 
-	len = ft_strlen(op);
-	write(1, op, len);
-	write(1, "\n", 1);
+	if (op != NULL)
+	{
+		len = ft_strlen(op);
+		write(1, op, len);
+		write(1, "\n", 1);
+	}
 }
 
 int	show_error_msg(void)

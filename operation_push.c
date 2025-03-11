18427318831b638b9b33e1e5c453c7e552ccbd12
Salvador Rioboo Naranjo <salrio74@gmail.com>
2025-03-11 08:48:44 +0100

    fix: change operation push to avoid double message

--- operation_push.c
+++ operation_push.c
@@ -6,7 +6,7 @@
 /*   By: srioboo- <srioboo-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 10:25:29 by srioboo-          #+#    #+#             */
-/*   Updated: 2025/03/10 15:11:55 by srioboo-         ###   ########.fr       */
+/*   Updated: 2025/03/11 08:47:28 by srioboo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 t_list *op_push(t_list *orig, t_list *dest, char *op)
 {
 	ft_lstadd_back(&dest, ft_lstnew(orig->content));
-	dest = op_rotate(dest, OP_ROTATE_A);
+	dest = op_rotate(dest, NULL);
 	show_op_msg(op);
 	return (dest);
 }

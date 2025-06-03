785f5d89ad96e5c2eb37ae895a3ce3dffd8e1182
srioboo <salrio74@gmail.com>
2025-06-03 11:57:56 +0200

    feat: improve test script

--- test_push_swap.sh
+++ test_push_swap.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+clear
+make re
+
 short_examples ()
 {
 	# set arguments
-	ARG="4 67 3"; 
+	ARG="4 67 3";
+	# main
+	echo -e "Launching './push_swap $ARG'"
+	./push_swap $ARG
 	# test
+	echo -e "\nLaunching './push_swap $ARG | wc -l'"
 	./push_swap $ARG | wc -l
 	# check result
+	echo -e "\nLaunching './push_swap $ARG | ./checker_linux $ARG'"
 	./push_swap $ARG | ./checker_linux $ARG
 }
 
